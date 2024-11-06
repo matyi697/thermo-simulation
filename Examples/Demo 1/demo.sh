d518e14043218e00300043af8c7551ebcd8c0256
matyi697 <wittmann.matyas@gmail.com>
2024-11-06 18:34:25 +0100

    modified:   .gitignore 	modified:   Examples/Demo 1/demo.sh 	new file:   Examples/Demo 1/polygons_1.txt 	new file:   Examples/Demo 1/polygons_2.txt 	renamed:    Examples/Demo 1/polygons.txt -> Examples/Demo 1/polygons_3.txt

--- Examples/Demo 1/demo.sh
+++ Examples/Demo 1/demo.sh
@@ -1,10 +1,10 @@
 gcc -o singleThread singleThread.c
 echo "Az egy szalon valo futas ideje: "
-time ./singleThread polygons.txt results_s.csv
+time ./singleThread polygons_1.txt results_s.csv
 
 nvcc -arch=sm_50 -o gpu gpu.cu
 echo "Az GPU futas ideje: "
-time ./gpu polygons.txt results_g.csv
+time ./gpu polygons_1.txt results_g.csv
 
 python plotter.py results_s.csv --title "CPU" --interpolation "bilinear"
 python plotter.py results_g.csv --title "GPU" --interpolation "bilinear"
